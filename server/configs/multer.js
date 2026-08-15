f1e62448a61d6ad0f781eeee2dc311856934792e
Vaibhav Gupta <vaibhav@Vaibhavs-MacBook-Air.local>
2026-08-15 20:16:26 +0530

    use os.tmpdir for multer uploads on Vercel serverless

--- server/configs/multer.js
+++ server/configs/multer.js
@@ -1,5 +1,13 @@
 import multer from "multer";
+import os from "os";
 
-const storage = multer.diskStorage({});
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, os.tmpdir());
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
 
 export const upload = multer({ storage });
